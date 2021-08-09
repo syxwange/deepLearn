bde1cadedfce1fd14f23f8b1e7dca3f4434755f4
syxwange <syxwange@163.com>
2021-08-10 00:54:45 +0800

    first commit

---
+++ src/liHongYi/hw3CNN.py
@@ -0,0 +1,8 @@
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder ,DatasetFolder
+from torch.utils.data import ConcatDataset, DataLoader, Subset,Dataset
+import torch
+from tqdm.auto import tqdm
+import torchvision
+
+
---
+++ src/liHongYi/tessdfsaf.py
@@ -0,0 +1,5 @@
+
+
+import wg
+
+aa = wg.WgTrain()
