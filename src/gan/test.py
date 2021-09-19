683a7fdbecc521b6c8d04ab66b417bc9173942a6
syxwange <syxwange@163.com>
2021-09-19 17:50:32 +0800

    add WGAN

---
+++ src/gan/test.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+
+        preprocess = nn.Sequential(
+            nn.Linear(128, 4*4*4*DIM),
+            nn.ReLU(True),
+        )
+        block1 = nn.Sequential(
+            nn.ConvTranspose2d(4*DIM, 2*DIM, 5),
+            nn.ReLU(True),
+        )
+        block2 = nn.Sequential(
+            nn.ConvTranspose2d(2*DIM, DIM, 5),
+            nn.ReLU(True),
+        )
+        deconv_out = nn.ConvTranspose2d(DIM, 1, 8, stride=2)
+
+        self.block1 = block1
+        self.block2 = block2
+        self.deconv_out = deconv_out
+        self.preprocess = preprocess
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, input):
+        output = self.preprocess(input)
+        output = output.view(-1, 4*DIM, 4, 4)
+        #print output.size()
+        output = self.block1(output)
+        #print output.size()
+        output = output[:, :, :7, :7]
+        #print output.size()
+        output = self.block2(output)
+        #print output.size()
+        output = self.deconv_out(output)
+        output = self.sigmoid(output)
+        #print output.size()
+        return output.view(-1, OUTPUT_DIM)
