ce22d6c1fc70bb252480b34c7de8a052576453d6
syxwange <syxwange@163.com>
2021-09-24 11:29:58 +0800

    Controllable GAN

--- src/gan/test.py
+++ src/gan/test.py
@@ -3,29 +3,21 @@ import torch.nn as nn
 
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+DIM = 64 # Model dimensionality
+BATCH_SIZE = 50 # Batch size
+CRITIC_ITERS = 5 # For WGAN and WGAN-GP, number of critic iters per gen iter
+LAMBDA = 10 # Gradient penalty lambda hyperparameter
+ITERS = 200000 # How many generator iterations to train for
+OUTPUT_DIM = 784 # Number of pixels in MNIST (28*28)
 
 class Generator(nn.Module):
     def __init__(self):
         super(Generator, self).__init__()
 
-        preprocess = nn.Sequential(
-            nn.Linear(128, 4*4*4*DIM),
-            nn.ReLU(True),
-        )
-        block1 = nn.Sequential(
-            nn.ConvTranspose2d(4*DIM, 2*DIM, 5),
-            nn.ReLU(True),
-        )
-        block2 = nn.Sequential(
-            nn.ConvTranspose2d(2*DIM, DIM, 5),
-            nn.ReLU(True),
-        )
-        deconv_out = nn.ConvTranspose2d(DIM, 1, 8, stride=2)
-
-        self.block1 = block1
-        self.block2 = block2
-        self.deconv_out = deconv_out
-        self.preprocess = preprocess
+        self.preprocess = nn.Sequential(nn.Linear(128, 4*4*4*DIM), nn.ReLU(True),)
+        self.block1 = nn.Sequential(nn.ConvTranspose2d(4*DIM, 2*DIM, 5), nn.ReLU(True) )
+        self.block2 = nn.Sequential(nn.ConvTranspose2d(2*DIM, DIM, 5), nn.ReLU(True), )
+        self.deconv_out = nn.ConvTranspose2d(DIM, 1, 8, stride=2)       
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, input):
@@ -42,3 +34,9 @@ class Generator(nn.Module):
         output = self.sigmoid(output)
         #print output.size()
         return output.view(-1, OUTPUT_DIM)
+
+
+G = Generator().to(device)
+nose = torch.randn((128),device=device)
+out = G(nose)
+print(out.shape)
